f05e57e5d40ae2d3cb21fe903b065b35c1b0c4e6
Marcolla <felipem.marcolla@gmail.com>
2023-03-12 16:58:31 -0300

    Criado o use case create-challenge-submission

--- src/domain/entities/submission.ts
+++ src/domain/entities/submission.ts
@@ -3,7 +3,7 @@ import { Entity } from "../../core/domain/Entity";
 type SubmissionProps = {
     challengeId: string;
     studentId: string;
-    createdAt: Date;
+    createdAt?: Date;
 };
 export class Submission extends Entity<SubmissionProps> {
     private constructor(props: SubmissionProps, id?: string){
@@ -11,7 +11,10 @@ export class Submission extends Entity<SubmissionProps> {
     }
 
     static create(props: SubmissionProps, id?: string){
-        const submission = new Submission(props, id);
+        const submission = new Submission({
+            ...props,
+            createdAt: props.createdAt ?? new Date()
+        }, id);
 
         return submission;
     }
